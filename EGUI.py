313722469f1afefd932639161d5fcb2ad7afe3c1
thereforguy <marcuspratley@gmail.com>
2014-01-20 22:32:17 +0000

    Comment changes
    Also, trying out the removal of lines.

--- EGUI.py
+++ EGUI.py
@@ -1,5 +1,4 @@
 from PyQt5 import QtCore, QtGui, uic
 class chem(QtGui.QWidget)
-    ...
-    #somewhere in constructor:
     uic.loadUi('chem.ui', self)
+    #SOME (ALL?) OF THIS FILE IS BROKEN. DO NOT USE.
