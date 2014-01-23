341edac3aba34b511aaf922f9a4a4fbb89bc3030
InvisibleTree <tomguitar123@hotmail.com>
2014-01-23 21:00:19 +0000

    Update MyInnumerableWealthofC++.cpp

--- MyInnumerableWealthofC++.cpp
+++ MyInnumerableWealthofC++.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
 
 int main(int argc, char* argv[]){
+	
+	char foo[] = {'H', 'E', 'L', 'L', 'O'};
+	char bar[] = {'W', 'O', 'R', 'L', 'D'};
 loop:
-	std::cout << "Hello world" << std::endl;
+	std::cout << foo << bar << std::endl;
 	goto loop;
 
 }
