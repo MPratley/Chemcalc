ab4bf08017f47378d51177ae503660a0e686f83c
InvisibleTree <tomguitar123@hotmail.com>
2014-04-04 14:42:27 +0100

    Working version1
    Deleted some old files that didn't deserve to reside in the git and now
    -rfm fully works i.e. you can look up the rfm of compounds such as H2SO4
    and the correct molar mass is outputted.

--- chemCalc.cpp
+++ chemCalc.cpp
@@ -1,6 +1,6 @@
 /*	chemCalc back-end
 	by InvisibleTree
-	quick compile = g++ chemCalc.cpp -o chemCalc
+	quick compile = g++ chemCalc1.cpp -o chemCalc1
 	
 	element data stored in elementData.txt
 */
@@ -17,13 +17,13 @@ struct element {																				//	Data structure for elements (read from tx
 	float massNo;																				//	Mass number
 }elementArray[118];
 
+int checkForNumber(char c, locale loc);
 int main(int argc, char* argv[]){
 
 	ifstream elementStream("elementData.txt");													//	Open ifstream for "elementData.txt" (needs to be in same folder as source and .exe)
 	
 	int massCounter = 0;																		//	Incrementing the RFM in the for loop
-	char c;
-	size_t pos;																					//	Position of element symbol in array
+	int massMultiplier = 1;																					//	Position of element symbol in array
 	locale loc;
 	
 	for(int i = 0; i < 118; i++){
@@ -51,29 +51,45 @@ int main(int argc, char* argv[]){
 		}	
 	}else if(string(argv[1]) == "-rfm" || string(argv[1]) == "--RFM"){							
 		for(int i = 0; i < 118; i++){													
-			pos = string(argv[2]).find(elementArray[i].symbol);
-			if(pos <= string(argv[2]).size()){													//	If the position of the char in the string is less than string size - using this reduces memory usage
-				if((pos+2) <= string(argv[2]).size()){											//	If the position + 2 of the char in the string is less than the string size i.e. not at the end
-					c = string(argv[2]).at(pos+1);												//	c is equal to the character at the position + 1 of the element symbol in the string
+			size_t pos = string(argv[2]).find(elementArray[i].symbol);
+			if(pos <= string(argv[2]).size()){													//	Check the position of the char is in the string
+				if((pos+2) <= string(argv[2]).size()){											//	Check if the position + 2 of the char in the string is less than the string size i.e. not at the end (end of string is counted, there
+					char c = string(argv[2]).at(pos+1);												//	c is equal to the character at the position + 1 of the element symbol in the string
 					if((elementArray[i].symbol).size() == 1 && islower(c,loc)){					//	If c is lower case e.g. the e in 'Fe', and if the element is a single letter symbol,
-						continue;																//	, do not add it to the mass counter (e.g. FeO = Fe + O + F instead of Fe + O)
+						continue;																//	, do not add it to the mass counter (e.g. FeO = Fe + O instead of F + Fe + O)
 					}else{
 						if(pos != string::npos){
-							massCounter += elementArray[i].massNo;
+							massMultiplier = checkForNumber(c, loc);
+							massCounter += massMultiplier * (elementArray[i].massNo);
+							massMultiplier = 1;																	// reset massCounter to 1
 						}
 					}
-				}else{
+				}else{																			// If the element/number if is the last in the string...
 					if(pos != string::npos){
-						massCounter += elementArray[i].massNo;
+							char c = string(argv[2]).at(pos);									// char c is set to the element/number
+							massMultiplier = checkForNumber(c, loc);
+							massCounter += massMultiplier * (elementArray[i].massNo);
+							massMultiplier = 1;																	// reset massCounter to 1
 					}
 				}
 			}else{
 				continue;
 			}
+			pos = 0;	// reset pos
+
 		}
 		cout << "The RFM of " << argv[2] << " is " << massCounter << "g/mol" << endl;
 	}
 	
 	return 0;
 }
+int checkForNumber(char c, locale loc){
 
+	int number = 1;
+	if(isdigit(c,loc) > 0){
+		number = c - '0';
+		return number;
+	}else{
+		return number;
+	}
+}
